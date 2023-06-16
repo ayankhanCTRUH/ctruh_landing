1e505e7482c9dd3b391f1b780060eb8579813ed3
ayankhan8223 <ayankhan8223@gmail.com>
2023-06-16 19:13:01 +0530

    added_galss

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,26 +14,50 @@ function Footer() {
         <section>
           <h5 className="sectionHeading">Company</h5>
           <ul>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Contact Us</li>
-            <li>Careers</li>
+            <li>
+              <a href="https://www.ctruh.com/aboutus">About</a>
+            </li>
+            <li>
+              <a href="https://www.ctruh.com/blogs">Blog</a>
+            </li>
+            <li>
+              <a href="https://www.ctruh.com/Contact-us">Contact Us</a>
+            </li>
+            <li>
+              <a href="https://in.linkedin.com/company/ctruh">Careers</a>
+            </li>
           </ul>
         </section>
         <section>
           <h5 className="sectionHeading">Community</h5>
           <ul>
-            <li>Instagram</li>
-            <li>Facebook</li>
-            <li>twitter</li>
-            <li>LinkedIn</li>
+            <li>
+              <a href="https://www.instagram.com/hello_ctruh/">Instagram</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/people/Ctruh/100090491642974/">
+                Facebook
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/HelloCtruh">twitter</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/company/ctruh/">LinkedIn</a>
+            </li>
           </ul>
         </section>
         <section>
           <h5 className="sectionHeading">Legal</h5>
           <ul>
-            <li>Terms of service</li>
-            <li>Privacy policy</li>
+            <li>
+              <a href="https://www.ctruh.com/terms-of-service">
+                Terms of service
+              </a>
+            </li>
+            <li>
+              <a href="https://www.ctruh.com/privacy-policy">Privacy policy</a>
+            </li>
           </ul>
         </section>
       </div>
